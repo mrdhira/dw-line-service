147e2b3cb96578462e1fb7753f5c9b77505dded7
Muhammad Dhira <t-muhammad.dhira@ovo.id>
2019-07-01 18:21:47 +0700

    update daily reminder logbook

--- api/controllers/cron/logbook/dailyReminderLogbook.js
+++ api/controllers/cron/logbook/dailyReminderLogbook.js
@@ -27,28 +27,47 @@ const getAllUser = () => {
   .then( (user) => user.data )
 }
 
+const setUser = async (users) => {
+  const userList = [];
+  let counter = 0;
+  await Promise.mapSeries((users), async (user) => {
+    counter++;
+    const checkLogbook = JSON.parse(
+      (
+        await logbookService.checkLogbook(user.cookie)
+      )
+      .body
+    );
+    const check = checkLogbook.message;
+    if (check.match(/You haven't filled activity on/)) {
+      console.log(counter);
+      console.log(user.line_id);
+      await userList.push(user.line_id);
+    }
+  });
+  return userList;
+};
+
 /**
- * The job that will be run
+ * The job that will be run 
  * @type {func}
  */
 const job = async () => {
   console.log(`CRON ${name} is running...`);
-  const userList = await getAllUser().filter((user) => user.isDailyReminder == 1);
-  return Promise.mapSeries(userList, async (user) => {
-    const checkLogbook = JSON.parse( ( await logbookService.checkLogbook(user.cookie) ).body );
-    const check = checkLogbook.message;
-    if (check.match(/You haven't filled activity on/)) {
-      const dateNow = Moment.tz('Asia/Jakarta').format('DD/MM/YY HH:mm:ss');      
-      const pushMessage = {
-        type: 'text',
-        text: `[ADMIN - DAILY REMINDER] - ${dateNow}\n`
-          + `${user.full_name} jangan lupa isi logbook ya^^`,
-      };
-      const form = { to:user.line_id, messages: [pushMessage] };
-      console.log(`Sending a reminder to fill logbook to: ${user.full_name}`);
-      return lineService.pushMessage(form, ACCESS);
-    }
-  });
+  const users = await getAllUser().filter((user) => user.isDailyReminder == 1);
+  const userList = await setUser(users);
+  const dateNow = await Moment.tz('Asia/Jakarta').format('DD/MM/YY HH:mm:ss');      
+  const pushMessage = {
+    type: 'text',
+    text: `[ADMIN - DAILY REMINDER] - ${dateNow}\n`
+      + `Kak jangan lupa isi logbook ya^^`,
+  };
+  const form = { to: userList, messages: [pushMessage] };
+  console.log(form);
+  return lineService.multiCastMessage(form, ACCESS)
+    .catch((error) => {
+      console.log(error);
+    })
 };
 
 module.exports = {
